589ed57ea4de7c7f20dbbfa36ef48d5ddf3817d1
mantianyinghuo1016 <1150432422@qq.com>
2021-02-06 19:47:55 +0800

    Revert "u"
    This reverts commit 467e276420fce014fd8c312bbc2be93c3302b10e.

--- ListNode.h
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-struct ListNode {
-	int val;
-	ListNode* next;
-	ListNode() :val(0), next(NULL) {};
-	ListNode(int x) : val(x), next(NULL) {};
-	ListNode(int x, ListNode* p) :val(x), next(p) {};
-};
