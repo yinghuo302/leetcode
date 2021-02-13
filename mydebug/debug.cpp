eb2285b54bfefe4077c246d64a8c8137cdbaca89
mantianyinghuo1016 <1150432422@qq.com>
2021-02-13 20:48:04 +0800

    gj

--- mydebug/debug.cpp
+++ mydebug/debug.cpp
@@ -1,5 +1,24 @@
 #include<bits/stdc++.h>
+#include "TreeNode.h"
 using namespace std;
+int kth_smallest(TreeNode*root,int &k){
+    if(!root)
+        return -1;
+    if(root->left){
+        int a=kth_smallest(root->left,k);
+        if(a!=-1)
+            return a;
+    }
+    --k;
+    if(k==0)
+        return root->val;
+    if(root->right){
+        int b=kth_smallest(root->right,k);
+        if(b!=-1)
+            return b;
+    }
+    return -1;
+}
 int main(){
     
     return 0;
