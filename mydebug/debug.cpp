1a899d982fe90cfaeeef083dd3a859d694d07ffc
mantianyinghuo1016 <1150432422@qq.com>
2021-02-13 20:28:21 +0800

    ignore

--- mydebug/debug.cpp
+++ mydebug/debug.cpp
@@ -1,21 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-struct ListNode {
-	int val;
-	ListNode* next;
-	ListNode() :val(0), next(NULL) {};
-	ListNode(int x) : val(x), next(NULL) {};
-	ListNode(int x, ListNode* p) :val(x), next(p) {};
-};
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 int main(){
     
     return 0;
