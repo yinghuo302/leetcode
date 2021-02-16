0c60ce435f5089c4fb956d32c60f6ad0f9f8eae8
mantianyinghuo1016 <1150432422@qq.com>
2021-02-16 21:15:52 +0800

    delete

--- leetcode.cpp
+++ leetcode.cpp
@@ -41,3 +41,144 @@ public:
         return num;
     }
 };
+//设计推特
+typedef int User_Id;
+typedef int Tweet_time;
+typedef int Tweet_Id;
+struct TweetNode{
+    Tweet_Id id;
+    Tweet_time time;
+    TweetNode*next;
+    TweetNode(int id,int time):id(id),time(time),next(NULL){}
+    TweetNode():id(-1),time(-1),next(NULL){}
+};
+class User{
+private:
+    TweetNode *tweets;
+public:
+    unordered_set<User_Id> followees;
+    User():tweets(NULL){}
+    User(int id):tweets(NULL){followees.emplace(id);}
+    void like(int like_id){
+        followees.emplace(like_id);
+    }
+    void dislike(int like_id){
+        int size=followees.size(),i=0;
+        unordered_set<int>::iterator followee=followees.begin();
+        while(followee!=followees.end())
+            if(like_id==*followee)
+                followees.erase(followee);
+    }
+    TweetNode *get_tweet(){
+        return tweets;
+    }
+    void post_tweet(int tweet_id,int time){
+        if(!tweets)
+            tweets=new TweetNode(tweet_id,time);
+        else
+            tweets->next=new TweetNode(tweet_id,time);
+    }
+};
+class Twitter {
+private:
+    unordered_map<User_Id,User*> users;
+    Tweet_time time;
+    TweetNode* mergeTwoLists(TweetNode* l1, TweetNode* l2) {
+	if (l1 == NULL)
+		return l2;
+	if (l2 == NULL)
+		return l1;
+	if (l1->time > l2->time) {
+		l1->next = mergeTwoLists(l1->next, l2); return l1;
+	}
+	else {
+		l2->next = mergeTwoLists(l1, l2->next); return l2;
+	}
+    }
+    TweetNode* deep_copy(TweetNode* head){
+        if(!NULL)
+            return NULL;
+        TweetNode* root=new TweetNode(head->id,head->time),*p=root;
+        while(head){
+            p->next=new TweetNode(head->id,head->time);p=p->next;
+        }
+        p=root->next;delete(root);
+        return p;
+    }
+    void deep_del(TweetNode *head){
+        if(!head)
+            return ;
+        TweetNode *p=head->next;
+        while(p){
+            delete(head);head=p;p=p->next;
+        }
+        delete(head);
+    }
+public:
+    Twitter() {
+        time=0;
+    }
+    void postTweet(int userId, int tweetId) {
+        if(users.empty()){
+            users.emplace(userId,new User(userId));
+            users[userId]->post_tweet(tweetId,time);return ;
+        }
+        unordered_map<User_Id,User*>::iterator usr_iter=users.find(userId);
+        if(users.end()==usr_iter){
+            users.emplace(userId,new User(userId));
+            usr_iter->second->post_tweet(tweetId,time);
+        }
+        else{
+            usr_iter->second->post_tweet(tweetId,time);
+        }
+        ++time;
+    }
+    vector<int> getNewsFeed(int userId) {
+        TweetNode *r=NULL;
+        for(auto &followee: users[userId]->followees){
+            r=mergeTwoLists(r,deep_copy(users[followee]->get_tweet()));
+        }
+        vector<int> res;TweetNode* h=r;
+        while(res.size()<=10&&r){
+            res.push_back(r->id);
+            r=r->next;
+        }
+        deep_del(h);return res;
+    }
+    void follow(int followerId, int followeeId) {
+        if(users.empty())
+            return ;
+        unordered_map<User_Id,User*>::iterator usr_iter=users.find(followerId);
+        if(users.end()!=usr_iter)
+            usr_iter->second->like(followeeId);
+    }
+    void unfollow(int followerId, int followeeId) {
+        if(users.empty())
+            return ;
+        unordered_map<User_Id,User*>::iterator usr_iter=users.find(followerId);
+        if(users.end()!=usr_iter)
+            usr_iter->second->dislike(followeeId);
+    }
+};
+//在数组中找两个数和为target
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int,int> m;int size=nums.size();
+    for(int i=0;i<size;++i){
+        unordered_map<int,int>::iterator a=m.find(target-nums[i]);
+        if(a!=m.end())
+            return {i,a->second};
+        m.emplace(nums[i],i);
+    }
+    return {};
+}
+//连续1的个数
+int findMaxConsecutiveOnes(vector<int>& nums) {
+    int size=nums.size();int res=0,count=0;
+    for(int i=0;i<size;++i){
+        if(nums[i])
+            ++count;
+        else if(count>res)
+            res=count;   
+    }
+    return (count>res)? count: res;
+}
