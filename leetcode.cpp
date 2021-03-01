f146eafbd3c26783c805088264c67073ae86cd35
zanilia1016 <76104215+zanilia1016@users.noreply.github.com>
2021-03-01 23:32:07 +0800

    3.1：1

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,23 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-int minPatches(vector<int> &nums, int n)
-{
+int minPatches(vector<int> &nums, int n){
 }
-int findMaximizedCapital(int k, int W, vector<int> &Profits, vector<int> &Capital)
-{
+int findMaximizedCapital(int k, int W, vector<int> &Profits, vector<int> &Capital){
 }
-int strStr(string haystack, string needle)
-{
+int strStr(string haystack, string needle){
     int h_size = haystack.size(), n_size = needle.size(), i = 0, j = 0;
-    while (j < n_size)
-    {
-        if (haystack[i] == needle[j])
-        {
+    while (j < n_size){
+        if (haystack[i] == needle[j]){
             ++i;
             ++j;
         }
-        else
-        {
+        else{
             i = i - j + 1;
             j = 0;
             if (i >= h_size)
@@ -27,18 +21,13 @@ int strStr(string haystack, string needle)
     return i - n_size;
 }
 //股票价格跨度，股票价格小于或等于今天价格的最大连续日数，包括今天
-class StockSpanner
-{
+class StockSpanner{
 private:
     stack<int> s;
     vector<int> prices;
-
 public:
-    StockSpanner()
-    {
-    }
-    int next(int price)
-    {
+    StockSpanner(){}
+    int next(int price){
         while (!s.empty() && price >= prices[s.top()])
             s.pop();
         int num = 0;
@@ -55,76 +44,63 @@ public:
 typedef int User_Id;
 typedef int Tweet_time;
 typedef int Tweet_Id;
-struct TweetNode
-{
+struct TweetNode{
     Tweet_Id id;
     Tweet_time time;
     TweetNode *next;
     TweetNode(int id, int time) : id(id), time(time), next(NULL) {}
     TweetNode() : id(-1), time(-1), next(NULL) {}
 };
-class User
-{
+class User{
 private:
     TweetNode *tweets;
-
 public:
     unordered_set<User_Id> followees;
     User() : tweets(NULL) {}
     User(int id) : tweets(NULL) { followees.emplace(id); }
-    void like(int like_id)
-    {
+    void like(int like_id){
         followees.emplace(like_id);
     }
-    void dislike(int like_id)
-    {
+    void dislike(int like_id){
         int size = followees.size(), i = 0;
         unordered_set<int>::iterator followee = followees.begin();
         while (followee != followees.end())
             if (like_id == *followee)
                 followees.erase(followee);
     }
-    TweetNode *get_tweet()
-    {
+    TweetNode *get_tweet(){
         return tweets;
     }
-    void post_tweet(int tweet_id, int time)
-    {
+    void post_tweet(int tweet_id, int time){
         if (!tweets)
             tweets = new TweetNode(tweet_id, time);
         else
             tweets->next = new TweetNode(tweet_id, time);
     }
 };
-class Twitter
-{
+class Twitter{
 private:
     unordered_map<User_Id, User *> users;
     Tweet_time time;
-    TweetNode *mergeTwoLists(TweetNode *l1, TweetNode *l2)
-    {
+    TweetNode *mergeTwoLists(TweetNode *l1, TweetNode *l2){
         if (l1 == NULL)
             return l2;
         if (l2 == NULL)
             return l1;
-        if (l1->time > l2->time)
-        {
+        if (l1->time > l2->time){
             l1->next = mergeTwoLists(l1->next, l2);
             return l1;
         }
-        else
-        {
+        else{
             l2->next = mergeTwoLists(l1, l2->next);
             return l2;
         }
     }
-    TweetNode *deep_copy(TweetNode *head)
-    {
+    TweetNode *deep_copy(TweetNode *head){
         if (!NULL)
             return NULL;
         TweetNode *root = new TweetNode(head->id, head->time), *p = root;
-        while (head)
-        {
+        while (head){
             p->next = new TweetNode(head->id, head->time);
             p = p->next;
         }
@@ -132,72 +108,58 @@ private:
         delete (root);
         return p;
     }
-    void deep_del(TweetNode *head)
-    {
+    void deep_del(TweetNode *head){
         if (!head)
             return;
         TweetNode *p = head->next;
-        while (p)
-        {
+        while (p){
             delete (head);
             head = p;
             p = p->next;
         }
         delete (head);
     }
-
 public:
-    Twitter()
-    {
+    Twitter(){
         time = 0;
     }
-    void postTweet(int userId, int tweetId)
-    {
-        if (users.empty())
-        {
+    void postTweet(int userId, int tweetId){
+        if (users.empty()){
             users.emplace(userId, new User(userId));
             users[userId]->post_tweet(tweetId, time);
             return;
         }
         unordered_map<User_Id, User *>::iterator usr_iter = users.find(userId);
-        if (users.end() == usr_iter)
-        {
+        if (users.end() == usr_iter){
             users.emplace(userId, new User(userId));
             usr_iter->second->post_tweet(tweetId, time);
         }
         else
-        {
             usr_iter->second->post_tweet(tweetId, time);
-        }
         ++time;
     }
-    vector<int> getNewsFeed(int userId)
-    {
+    vector<int> getNewsFeed(int userId){
         TweetNode *r = NULL;
-        for (auto &followee : users[userId]->followees)
-        {
+        for (auto &followee : users[userId]->followees){
             r = mergeTwoLists(r, deep_copy(users[followee]->get_tweet()));
         }
         vector<int> res;
         TweetNode *h = r;
-        while (res.size() <= 10 && r)
-        {
+        while (res.size() <= 10 && r){
             res.push_back(r->id);
             r = r->next;
         }
         deep_del(h);
         return res;
     }
-    void follow(int followerId, int followeeId)
-    {
+    void follow(int followerId, int followeeId){
         if (users.empty())
             return;
         unordered_map<User_Id, User *>::iterator usr_iter = users.find(followerId);
         if (users.end() != usr_iter)
             usr_iter->second->like(followeeId);
     }
-    void unfollow(int followerId, int followeeId)
-    {
+    void unfollow(int followerId, int followeeId){
         if (users.empty())
             return;
         unordered_map<User_Id, User *>::iterator usr_iter = users.find(followerId);
@@ -206,12 +168,10 @@ public:
     }
 };
 //在数组中找两个数和为target
-vector<int> twoSum(vector<int> &nums, int target)
-{
+vector<int> twoSum(vector<int> &nums, int target){
     unordered_map<int, int> m;
     int size = nums.size();
-    for (int i = 0; i < size; ++i)
-    {
+    for (int i = 0; i < size; ++i){
         unordered_map<int, int>::iterator a = m.find(target - nums[i]);
         if (a != m.end())
             return {i, a->second};
@@ -220,12 +180,10 @@ vector<int> twoSum(vector<int> &nums, int target)
     return {};
 }
 //连续1的个数
-int findMaxConsecutiveOnes(vector<int> &nums)
-{
+int findMaxConsecutiveOnes(vector<int> &nums){
     int size = nums.size();
     int res = 0, count = 0;
-    for (int i = 0; i < size; ++i)
-    {
+    for (int i = 0; i < size; ++i){
         if (nums[i])
             ++count;
         else if (count > res)
@@ -650,15 +608,10 @@ public:
 class LFUCache {
 public:
     LFUCache(int capacity) {
-
     }
-    
     int get(int key) {
-
     }
-    
     void put(int key, int value) {
-
     }
 };
 //判断是否为单调数组
@@ -706,6 +659,7 @@ int myAtoi(string s) {
     }
     return res;
 }
+//是否为回文数
 bool isPalindrome(int x) {
     if(x<0||(!x&&x%10==0))
         return false;
@@ -716,7 +670,7 @@ bool isPalindrome(int x) {
     }
     return x==reversed||x==(reversed%10);
 }
-//
+//求数组pos为i到j的元素之和
 class NumArray {
 private:
     vector<int> sums;
